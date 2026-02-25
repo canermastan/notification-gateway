ae96a38515794cb76a324599e4d85826ddfc440e
canermastan <jcanermastan@gmail.com>
2026-02-25 18:19:44 +0300

    perf(db): add index to apiKey for faster project lookups

--- src/main/java/com/canermastan/notificationgateway/entity/Project.java
+++ src/main/java/com/canermastan/notificationgateway/entity/Project.java
@@ -1,16 +1,16 @@
 package com.canermastan.notificationgateway.entity;
 
 import io.quarkus.hibernate.reactive.panache.PanacheEntity;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 
 import java.util.List;
 
 @Entity
+@Table(indexes = @Index(columnList = "apiKey"))
 public class Project extends PanacheEntity {
     public String name;
+
+    @Column(unique = true)
     public String apiKey;
     public boolean isActive = true;
 
